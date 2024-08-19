8523299c83d856f13ee4b02e07f83c41529e91ca
PashaAmeri <a.pasha.ameri@gmail.com>
2024-08-19 14:51:23 +0330

    user table migration and model added

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'phone',
+        'bio'
     ];
 
     /**
@@ -28,7 +29,6 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
         'remember_token',
     ];
 
@@ -41,7 +41,6 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
         ];
     }
 }
